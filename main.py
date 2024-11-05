9fc39a252b883515bce0c99dee80b7355aba44a0
hyperdb <hyperdb@gmail.com>
2024-11-06 01:02:28 +0900

    ✨経過および処理をstdoutに出力

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 import glob
 import shutil
+import sys
 from config import conf
 
 
 def main(in_path: str, out_path: str):
+    skip_cnt = 0
+    copy_cnt = 0
+
     filelist = glob.glob(os.path.join(in_path, "**/*"), recursive=True)
     for f in filelist:
         if not os.path.isfile(f):
@@ -16,7 +20,18 @@ def main(in_path: str, out_path: str):
                 os.makedirs(dir_name)
             if not os.path.isfile(target_name):
                 shutil.copy2(f, target_name)
+                sys.stdout.write('\r\033[K' + target_name)
+                sys.stdout.flush()
+                copy_cnt += 1
+            else:
+                sys.stdout.write('\r\033[K' + "skip")
+                sys.stdout.flush()
+                skip_cnt += 1
 
+    sys.stdout.write('\r\033[K')
+    sys.stdout.write(f'{copy_cnt} file(s) copy\n')
+    sys.stdout.write(f'{skip_cnt} file(s) skip\n')
+    sys.stdout.flush()
 
 if __name__ == "__main__":
 
